3fb5d3754c88e39278f54c6abb1fb11a73993320
himanniii20 <himanikhobragade24@gmail.com>
2023-10-28 14:13:23 +0530

    Responsive images

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="content">
-    <img alt="content image 1" src="../assets/image1.png">
-    <img alt="content image 2" src="../assets/image2.png">
-    <img alt="content image 3" src="../assets/image3.png">
-    <img alt="content image 4" src="../assets/image4.png">
-    <img alt="content image 5" src="../assets/image5.png">
-    <img alt="content image 6" src="../assets/image6.png">
-    <img alt="content image 7" src="../assets/image7.png">
+  <div class="content row">
+    <img alt="content image 1" src="../assets/image1.png" class="col-md-6 p-0 col-sm-12">
+    <img alt="content image 2" src="../assets/image2.png" class="col-md-6 p-0 col-sm-12">
+    <img alt="content image 3" src="../assets/image3.png" class="col-md-6 p-0 col-sm-12">
+    <img alt="content image 4" src="../assets/image4.png" class="col-md-6 p-0 col-sm-12">
+    <img alt="content image 5" src="../assets/image5.png" class="col-md-6 p-0 col-sm-12">
+    <img alt="content image 6" src="../assets/image6.png" class="col-md-6 p-0 col-sm-12">
+    <img alt="content image 7" src="../assets/image7.png" class="col-xs-12 p-0">
   </div>
 </template>
   
